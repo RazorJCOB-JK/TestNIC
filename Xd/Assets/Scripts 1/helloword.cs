048001209038857ce1ae0427281ab0201ab11566
RazorJCOB-JK <j.kozka@interia.eu>
2019-10-04 13:02:25 +0200

    aktualizacja projektu

---
+++ Xd/Assets/Scripts 1/helloword.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int min = 1;
+        int max = 1000;
+        print("witaj w liczbowym czarodzieju");
+        print("wybierz liczbe z przedziału 1 do 100");
+        print("maksymalna liczba jaka mozesz wybrac to" + max);
+        print("minimalna liczba jaka mozesz wybrac to" + min);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
